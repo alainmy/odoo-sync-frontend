005cbd254e1b43dcc703a6553f4b9a81791eaa82
Alain Alvarez <alalvarez@soaint.com>
2026-09-21 20:49:22 -0400

    Cambios

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-const proxy = {
-  '/api':         { target: 'http://fastapi:5010', changeOrigin: true },
-  '/auth':        { target: 'http://fastapi:5010', changeOrigin: true },
-  '/odoo':        { target: 'http://fastapi:5010', changeOrigin: true },
-  '/woocommerce': { target: 'http://fastapi:5010', changeOrigin: true },
+const devProxy = {
+  '/api':          { target: 'http://fastapi:5010', changeOrigin: true },
+  '/auth/':        { target: 'http://fastapi:5010', changeOrigin: true },
+  '/odoo/':        { target: 'http://fastapi:5010', changeOrigin: true },
+  '/woocommerce/': { target: 'http://fastapi:5010', changeOrigin: true },
 }
 
 export default defineConfig({
@@ -17,12 +17,11 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3007,
-    proxy,
+    proxy: devProxy,
   },
   preview: {
     host: '0.0.0.0',
     port: 4173,
     allowedHosts: ['spicebox-alinea.cumbre.ar'],
-    proxy, // <-- esto es lo que faltaba
   },
 })
